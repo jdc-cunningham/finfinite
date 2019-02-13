94c3cf47a0cae89c62e23e64a1c7ae57ffe6b9c0
Jacob David Cunningham <jcun4128@gmail.com>
2019-02-12 21:32:42 -0600

    read through form doc, got some idea of what's going on, still figuring out setState and setting callbacks states for error, issue with setState currently

--- reactjs-django/frontend/src/CreditCard/AddCreditCard.js
+++ reactjs-django/frontend/src/CreditCard/AddCreditCard.js
@@ -1,40 +1,125 @@
 import React, { Component } from 'react';
 
 class AddCreditCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            errFields: [],
+            fieldValues: {}
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    throwError() {
+
+    }
+
+    isEmpty(name, value) {
+        if (value.length) {
+            let arrIndex = this.state.errFields.indexOf(name);
+            this.setState.errFields.splice(arrIndex, 1);
+        }
+        else {
+            this.setState.errFields.push(name);
+        }
+    }
+
+    validateField(name, value) {
+        switch (name) {
+            case 'card-name':
+                this.isEmpty(value);
+                break;
+            default :
+                break;
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        const formValues = this.state;
+        // const validateFcn = this.validateField;
+
+        // can't access super, this refers to object
+        // Object.keys(this.state).map(function (key) {
+        //     validateFcn.validateField(key, formValues[key]);
+        // });
+
+        Object.keys(this.state).map((key) => (
+            this.validateField(key, formValues[key])
+        ))
+
+        console.log(this.state.errFields);
+
+        // alert('Yo bitch: ' + this.state['card-name'].value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <form>
                 <div className="AddCreditCard-form-row">
                     <label>
                         Card name
-                        <input name="card-name" type="text" />
+                        <input 
+                            type="text"
+                            name="card-name"
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label>
                 </div>
                 <div className="AddCreditCard-form-row">
                     <label>
                         Card balance
-                        <input type="number" step="0.01" />
+                        <input 
+                            type="number" 
+                            name="card-balance"
+                            step="0.01" 
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label>
                 </div>
                 <div className="AddCreditCard-form-row">
                     <label>
                         Card credit limit
-                        <input type="number" step="0.01" />
+                        <input 
+                            type="number" 
+                            name="card-credit-limit"
+                            step="0.01"
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label>
                 </div>
                 <div className="AddCreditCard-form-row">
                     <label>
                         Card due date
-                        <input type="number" max-length="2" max-value="31" />
+                        <input
+                            type="number"
+                            name="card-due-date"
+                            max-length="2"
+                            max-value="31"
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label>
                 </div>
                 <div className="AddCreditCard-form-row">
                     <label>
                         Card APR
-                        <input type="number" step="0.01" />
+                        <input 
+                            type="number" 
+                            name="card-apr"
+                            step="0.01"
+                            value={this.state.value}
+                            onChange={this.handleChange} />
                     </label>
                 </div>
-                <button type="button">Add Card</button>
+                <button type="button" onClick={this.handleSubmit}>Add Card</button>
             </form>
         );
     }
