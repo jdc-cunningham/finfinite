ac8dd7750216e8267dec16bb2be7c0fb54510b53
Jacob David Cunningham <jcun4128@gmail.com>
2019-03-12 22:17:41 -0500

    basic empty/focus validation all fields

--- reactjs-django/frontend/src/CreditCard/AddCreditCard.js
+++ reactjs-django/frontend/src/CreditCard/AddCreditCard.js
@@ -6,7 +6,12 @@ class AddCreditCard extends Component {
 		super(props);
 		this.state = {
 			errFields: [],
-			['card-name']: ''
+			'card-name': '',
+			'card-balance': '',
+			'card-credit-limit': '',
+			'card-due-date': '',
+			'card-apr': '',
+			'card-annual-fee': '',
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -41,6 +46,21 @@ class AddCreditCard extends Component {
 			case 'card-name':
 				this.isEmpty(name, value);
 				break;
+			case 'card-balance':
+				this.isEmpty(name, value);
+				break;
+			case 'card-credit-limit':
+				this.isEmpty(name, value);
+				break;
+			case 'card-due-date':
+				this.isEmpty(name, value);
+				break;
+			case 'card-apr':
+				this.isEmpty(name, value);
+				break;
+			case 'card-annual-fee':
+				this.isEmpty(name, value);
+				break;
 			default:
 				break;
 		}
@@ -53,24 +73,39 @@ class AddCreditCard extends Component {
 	}
 
 	handleFocus(event) {
-		let curFormState = this.state;
-		console.log(curFormState);
-		let curErrFields = curFormState.errFields;
+		console.log('focus');
+		let curErrFields = this.state.errFields;
 		let arrIndex = curErrFields.indexOf(event.target.name);
-
 
 		// // if ( this.state.errFields.indexOf( event.name !== -1 ) {
 		if ( curErrFields.length && arrIndex > -1 ) {
-			this.setState({
-				errFields: curErrFields.splice(arrIndex, 1)
-			})
+			curErrFields.splice(arrIndex, 1);
+
+			this.setState({ curErrFields });
+			// curErrFields.splice(arrIndex, 1);
 		}
 
-		this.setState(curFormState); // re-render
+		console.log(curErrFields);
+
+		// this.setState(curFormState); // re-render
+
+		// const formValues = this.state;
+		// // const validateFcn = this.validateField;
+
+		// // can't access super, this refers to object
+
+		// Object.keys(this.state).map((key) => (
+		// 	this.validateField(key, formValues[key])
+		// ))
+
+		// this.setState(formValues); // re-render to fire validation
+
+		// console.log(this.state);
 	}
 
 	handleSubmit(event) {
 		const formValues = this.state;
+		console.log(formValues);
 		// const validateFcn = this.validateField;
 
 		// can't access super, this refers to object
@@ -85,8 +120,7 @@ class AddCreditCard extends Component {
 		this.setState(formValues); // re-render to fire validation
 
 		console.log(this.state);
-
-		// alert('Yo bitch: ' + this.state['card-name'].value);
+		
 		event.preventDefault();
 	}
 
@@ -111,54 +145,75 @@ class AddCreditCard extends Component {
 							name="card-name"
 							// value={this.state["card-name"]}
 							onFocus={this.handleFocus}
-							onChange={this.handleChange} />
+							onChange={this.handleChange}
+							style={{
+								/* property: value */
+								/* can put styles */
+							}} />
 					</label>
 				</div>
-				{/* <div className="form-row">
-										<label>
-												Card balance
-												<input 
-														type="number" 
-														name="card-balance"
-														step="0.01" 
-														value={this.state.value}
-														onChange={this.handleChange} />
-										</label>
-								</div>
-								<div className="form-row">
-										<label>
-												Card credit limit
-												<input 
-														type="number" 
-														name="card-credit-limit"
-														step="0.01"
-														value={this.state.value}
-														onChange={this.handleChange} />
-										</label>
-								</div>
-								<div className="form-row">
-										<label>
-												Card due date
-												<input
-														type="number"
-														name="card-due-date"
-														max-length="2"
-														max-value="31"
-														value={this.state.value}
-														onChange={this.handleChange} />
-										</label>
-								</div>
-								<div className="form-row">
-										<label>
-												Card APR
-												<input 
-														type="number" 
-														name="card-apr"
-														step="0.01"
-														value={this.state.value}
-														onChange={this.handleChange} />
-										</label>
-								</div> */}
+				<div className={'form-row' + (hasError('card-balance') ? ' error' : '')}>
+					<label>
+						Card balance
+						<input 
+								type="number" 
+								name="card-balance"
+								step="0.01" 
+								value={this.state.value}
+								onFocus={this.handleFocus}
+								onChange={this.handleChange} />
+					</label>
+				</div>
+				<div className={'form-row' + (hasError('card-credit-limit') ? ' error' : '')}>
+					<label>
+						Card credit limit
+						<input 
+								type="number" 
+								name="card-credit-limit"
+								step="0.01"
+								value={this.state.value}
+								onFocus={this.handleFocus}
+								onChange={this.handleChange} />
+					</label>
+				</div>
+				<div className={'form-row' + (hasError('card-due-date') ? ' error' : '')}>
+					<label>
+						Card due date
+						<input
+								type="number"
+								name="card-due-date"
+								placeholder="day of month"
+								maxLength={2}
+								max={31}
+								value={this.state.value}
+								onFocus={this.handleFocus}
+								onChange={this.handleChange} />
+					</label>
+				</div>
+				<div className={'form-row' + (hasError('card-apr') ? ' error' : '')}>
+					<label>
+						Card APR
+						<input 
+								type="number" 
+								name="card-apr"
+								step="0.01"
+								value={this.state.value}
+								onFocus={this.handleFocus}
+								onChange={this.handleChange} />
+					</label>
+				</div>
+				<div className={'form-row' + (hasError('card-annual-fee') ? ' error' : '')}>
+					<label>
+						Card Annual Fee
+						<input 
+								type="number" 
+								name="card-annual-fee"
+								step="0.01"
+								value={this.state.value}
+								onFocus={this.handleFocus}
+								onChange={this.handleChange} />
+					</label>
+				</div>
 				<div className="form-row center">
 					<button type="button" onClick={this.handleSubmit}>Add Card</button>
 				</div>
